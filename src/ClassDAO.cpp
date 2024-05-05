577622dc70a6a71fc840464fcb9487f5a198e1ea
Gabriel Gato <70043907+Gato-Gabriel@users.noreply.github.com>
2024-05-05 01:04:53 -0300

    DAO adequados ao DTO - mas ainda erros pendentes

--- src/ClassDAO.cpp
+++ src/ClassDAO.cpp
@@ -6,6 +6,38 @@ class ClassDAO : public AbstractDAO<ClassDTO> {
     vector<ClassDTO> classes;
 
     public:
+    //getters
+    string getCode() { return code; }
+    string getName() { return name; }
+    string getSyllabus(){ return syllabus; }
+    int getSemesterNumber(){ return semesterNumber; }
+    int getyear(){ return year; }
+    vector<string> getStudentsRa(){ return studentsRA; }
+    map<string, double> getStudentGrades(){ return studentGrades; }
+
+    //setters
+    void setCode(string code){
+        this->code = code;
+    }
+    void setName(string name){
+        this->name = name;
+    }    
+    void setSyllabus(string syllabus){
+        this->syllabus = syllabus;
+    }
+    void setYear(int year){
+        this->year = year;
+    }
+    void setSemesterNumber(int semesterNumber){
+        this->semesterNumber = semesterNumber;
+    }
+    void setStudentsRA(vector<string> studentsRA) {
+        this->studentsRA = studentsRA;
+    }
+    void setStudentGrades(map<string, double> studentGrades) {
+        this->studentGrades = studentGrades;
+    }
+
     void add(const ClassDTO& classInfo) override {
         classes.push_back(classInfo);
         cout << "Adicionando turma "<<classInfo.getNome()<<" ao sistema..." <<endl;
