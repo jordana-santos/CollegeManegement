9a5bdd149ab7a822eaec7c9e949ced4b0490aceb
emily <emilytomadon@gmail.com>
2024-06-07 01:04:35 +0100

    menu

--- menu.hpp
+++ menu.hpp
@@ -1,10 +1,49 @@
-#ifndef MENU_HPP
-#define MENU_HPP
+#ifndef MENU_H_
+#define MENU_H_
 
-class Menu {
-public:
-    void showMenu();
-    void option(int choice);
-};
+#include <iostream>
+#include <vector>
 
-#endif 
+using namespace std;
+
+class Menu final
+	{
+	private:
+		vector<string> &itens;
+
+		string title;
+		string message;
+
+		string symbol;
+		int width;
+		bool repeatList;
+		bool zeroForLastOpt;
+
+		const bool isNotAValidChoice(unsigned long choice) const;
+		const string replicate(string text, int times) const;
+		const string makeDecorator();
+
+	public:
+		Menu(vector<string> &itens, string title = "Menu", string message = "Option: ");
+		virtual ~Menu();
+
+		const int getChoice();
+		void setDecorator(const string &decorator, int width);
+
+		const string& getDecorator() const;
+		const string& getMessage() const;
+		const int getLength() const;
+		void setMessage(const string &message);
+		bool isRepeatList() const;
+		void setRepeatList(bool repeatList = false);
+		const string getSymbol() const;
+		void setSymbol(const string &symbol = "-");
+		const string& getTitle() const;
+		void setTitle(const string &title);
+		int getWidth() const;
+		void setWidth(int width = 0);
+		bool isZeroForLastOpt() const;
+		void setZeroForLastOpt(bool zeroForLastOpt = true);
+	};
+
+#endif /* MENU_H_ */
